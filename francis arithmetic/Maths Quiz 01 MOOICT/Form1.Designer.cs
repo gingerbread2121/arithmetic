56d60eca2c898671892beef9d3daaaa406cfc523
gingerbread2121 <160980356+gingerbread2121@users.noreply.github.com>
2024-04-06 16:19:58 +0800

    Add files via upload

---
+++ francis arithmetic/Maths Quiz 01 MOOICT/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Francis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblNumA = new System.Windows.Forms.Label();
+            this.lblSymbol = new System.Windows.Forms.Label();
+            this.lblNumB = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.Red;
+            this.lblScore.Location = new System.Drawing.Point(9, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(115, 31);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblNumA
+            // 
+            this.lblNumA.AutoSize = true;
+            this.lblNumA.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumA.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumA.Location = new System.Drawing.Point(111, 155);
+            this.lblNumA.Name = "lblNumA";
+            this.lblNumA.Size = new System.Drawing.Size(49, 33);
+            this.lblNumA.TabIndex = 1;
+            this.lblNumA.Text = "00";
+            // 
+            // lblSymbol
+            // 
+            this.lblSymbol.AutoSize = true;
+            this.lblSymbol.BackColor = System.Drawing.Color.Transparent;
+            this.lblSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSymbol.Location = new System.Drawing.Point(156, 155);
+            this.lblSymbol.Name = "lblSymbol";
+            this.lblSymbol.Size = new System.Drawing.Size(33, 33);
+            this.lblSymbol.TabIndex = 2;
+            this.lblSymbol.Text = "+";
+            // 
+            // lblNumB
+            // 
+            this.lblNumB.AutoSize = true;
+            this.lblNumB.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumB.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumB.Location = new System.Drawing.Point(195, 155);
+            this.lblNumB.Name = "lblNumB";
+            this.lblNumB.Size = new System.Drawing.Size(32, 33);
+            this.lblNumB.TabIndex = 3;
+            this.lblNumB.Text = "?";
+            this.lblNumB.Click += new System.EventHandler(this.lblNumB_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(230, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "=";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.BackColor = System.Drawing.Color.Transparent;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblAnswer.Location = new System.Drawing.Point(9, 50);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(104, 31);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "Correct";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.BackColor = System.Drawing.Color.Azure;
+            this.txtAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.ForeColor = System.Drawing.Color.Black;
+            this.txtAnswer.Location = new System.Drawing.Point(280, 152);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(131, 40);
+            this.txtAnswer.TabIndex = 6;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.CheckAnswer);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(201, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "TRY";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.CheckButtonClickEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.BackgroundImage = global::Maths_Quiz_01_MOOICT.Properties.Resources._1uKNlo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(500, 345);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNumB);
+            this.Controls.Add(this.lblSymbol);
+            this.Controls.Add(this.lblNumA);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Arithmetic Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblNumA;
+        private System.Windows.Forms.Label lblSymbol;
+        private System.Windows.Forms.Label lblNumB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ francis arithmetic/Maths Quiz 01 MOOICT/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Francis
+{
+    
+    public partial class Form1 : Form
+    {
+
+        Random rnd = new Random();
+        string[] Maths = {"Addition", "Subtraction", "Multiply" , "Division"};
+        int total;
+        int score;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtAnswer.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                txtAnswer.Text = txtAnswer.Text.Remove(txtAnswer.Text.Length - 1);
+            }
+        }
+
+        private void CheckButtonClickEvent(object sender, EventArgs e)
+        {
+
+            int userEntered = Convert.ToInt32(txtAnswer.Text);
+
+            if (userEntered == total)
+            {
+                lblAnswer.Text = "Correct";
+                lblAnswer.ForeColor = Color.Green;
+                score += 1;
+                lblScore.Text = "Score: " + score;
+                SetUpGame();
+
+            }
+            else
+            {
+                lblAnswer.Text = "Incorrect";
+                lblAnswer.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void SetUpGame()
+        {
+            int numA = rnd.Next(10, 20);
+            int numB = rnd.Next(0, 9);
+
+            txtAnswer.Text = null;
+
+            switch (Maths[rnd.Next(0, Maths.Length)])
+            {
+                case "Addition":
+                    total = numA + numB;
+                    lblSymbol.Text = "+";
+                    lblSymbol.ForeColor = Color.Red;
+                    break;
+
+                case "Subtraction":
+                    total = numA - numB;
+                    lblSymbol.Text = "-";
+                    lblSymbol.ForeColor = Color.Brown;
+                    break;
+
+                case "Multiply":
+                    total = numA * numB;
+                    lblSymbol.Text = "x";
+                    lblSymbol.ForeColor = Color.Green;
+                    break;
+                case "Division":
+                    total = numA / numB;
+                    lblSymbol.Text = "/";
+                    lblSymbol.ForeColor = Color.Blue;
+                    break;
+            }
+
+            lblNumA.Text = numA.ToString();
+            lblNumB.Text = numB.ToString();
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNumB_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
